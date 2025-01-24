9a5eb375a8a9b8fa1e91d3d183643aefdfd7a6fd
Caberu <lecaberudo@gmail.com>
2025-01-24 22:02:24 +0100

    Dit metre, metre, j'ai perdu

--- DungeonCrawling/crawling.gd
+++ DungeonCrawling/crawling.gd
@@ -1,7 +1,10 @@
-extends Node2D
+extends Node3D
 
 var player_coord : Vector2 = Vector2(0,0)
 var player_ori : Vector2 = Vector2(0,1)
+var room_length := 10
+
+@export var cam_center : Node3D
 
 func _process(delta):
 	if (Input.is_action_just_pressed("rotate_left")):
@@ -11,6 +14,9 @@ func _process(delta):
 	if (Input.is_action_just_pressed("forward")):
 		forward()
 	
+	cam_center.rotation = Vector3(0,player_ori.angle(),0)
+	cam_center.position = Vector3(-player_coord.y,0,-player_coord.x)*room_length
+	
 	# DebugMovesVisual
 	#queue_redraw()
 
@@ -18,9 +24,9 @@ func forward():
 	player_coord += player_ori
 
 func rotate_view(clock_wise := true):
-	if (!clock_wise):
+	if (clock_wise):
 		player_ori = Vector2(player_ori.y*-1, player_ori.x)
-	elif (clock_wise):
+	elif (!clock_wise):
 		player_ori = Vector2(player_ori.y, player_ori.x*-1)
 
 #func _draw():
