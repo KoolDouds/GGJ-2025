c3d157c870e5054aab76376aea042f04fda3ad79
Giovanni Karra <karragiovanni@gmail.com>
2025-01-25 11:15:49 +0100

    combat in 3D

--- Script/combat_controls.gd
+++ Script/combat_controls.gd
@@ -1,10 +1,22 @@
+class_name Battle
 extends Node2D
 
+@onready var enemy_res := preload("res://enemy_proto.tscn")
+var enemy : Node2D
+
+func _ready() -> void:
+	print(get_tree().get_root().get_children())
+	var hands = get_tree().get_root().find_child("Hands", true, false)
+	hands.manager = $"."
+	enemy = enemy_res.instantiate()
+	enemy.position = Vector2(300, 200)
+	add_child(enemy)
+
 func hit(dmg: int, side: Hand.SIDE):
-	var enemy := $"..".find_child("enemy")
-	if enemy != null:
-		print("gave " + str(dmg) + " damage")
-		enemy.hp -= dmg
+	#var enemy := $"..".find_child("enemy")
+	#if enemy != null:
+	print("gave " + str(dmg) + " damage")
+	enemy.hp -= dmg
 
 func get_hit(dmg: int, side: Hand.SIDE):
 	for child in get_tree().get_nodes_in_group("hand"):
--- Script/enemy_temp.gd
+++ Script/enemy_temp.gd
@@ -1,7 +1,8 @@
 extends Node2D
 
 @onready var hp_label: Label = $"HP"
-@onready var battle := $"../battle"
+@onready var battle := $".."
+@onready var anim : AnimationPlayer = $"AnimationPlayer"
 
 @export var moves : Array[EnemyMove]
 var index := 0
@@ -24,6 +25,11 @@ func attack():
 	while true:
 		var move := moves[index]
 		print("awaiting attack...")
+		anim.speed_scale = 1/move.prep_time
+		if move.side == Hand.SIDE.LEFT:
+			anim.play("attack_left")
+		else:
+			anim.play("attack_right")
 		await get_tree().create_timer(move.prep_time).timeout
 		battle.get_hit(move.dmg, move.side)
 		await get_tree().create_timer(move.cooldown).timeout
