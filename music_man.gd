3a1f3addee5094e02899befad60c54e3589129eb
Caberu <lecaberudo@gmail.com>
2025-01-26 13:31:44 +0100

    Son adaptatif

---
+++ music_man.gd
@@ -0,0 +1,23 @@
+extends Node2D
+
+@export var theme_calme : AudioStreamPlayer2D
+var base_vol_calme : float
+@export var theme_dur : AudioStreamPlayer2D
+var base_vol_dur : float
+@export var theme_inv : AudioStreamPlayer2D
+var base_vol_inv : float
+
+var hands : Hands
+var inv : Inventory
+
+func _ready():
+	inv = get_tree().get_first_node_in_group("inventory")
+	hands = get_tree().get_first_node_in_group("hands")
+	base_vol_calme = theme_calme.volume_db
+	base_vol_dur = theme_dur.volume_db
+	base_vol_inv = theme_inv.volume_db
+
+func _process(delta):
+	theme_calme.volume_db = -1000 if hands.manager !=null or inv.opened else base_vol_calme
+	theme_dur.volume_db = -1000 if hands.manager == null or inv.opened else base_vol_dur
+	theme_inv.volume_db = -1000 if hands.manager != null or !inv.opened else base_vol_inv
