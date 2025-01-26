d6e98c542f5a3fc70ecfb9e399e50923a3a3804e
Caberu <lecaberudo@gmail.com>
2025-01-26 04:18:11 +0100

    Bubble

--- DungeonCrawling/crawling.gd
+++ DungeonCrawling/crawling.gd
@@ -41,6 +41,7 @@ func _ready():
 			continue
 		var coord = pos_to_coord(i.position)
 		rooms[coord] = i
+		print(coord)
 		
 		var vec = Vector2.UP
 		for xx in range(4):
@@ -53,16 +54,7 @@ func _ready():
 					doors[door_coord] = door
 					$Center.add_child(door)
 					print(door_coord)
-			vec = rotate_vector_90d(vec)
-	
-	for i in room_list:
-		if (!i is Room):
-			continue
-		var coord = pos_to_coord(i.position)
-		var vec = Vector2.UP
-		for xx in range(4):
-			if (!rooms.has(coord+vec)):
-				print(coord+vec)
+			else:
 				var door = load("res://bouche_trou.tscn").instantiate()
 				door.rotation = Vector3(0,vec.angle(),0)
 				i.add_child(door)
