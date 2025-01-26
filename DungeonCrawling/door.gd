c27b5726d5a536d7c3b860a122e6c52a2c87d0ff
Caberu <lecaberudo@gmail.com>
2025-01-26 04:29:45 +0100

    Dor

--- DungeonCrawling/door.gd
+++ DungeonCrawling/door.gd
@@ -1,6 +1,12 @@
 class_name Door extends Node3D
 
 @export var open := true
+@export var open_gfx : Node3D
+@export var closed_gfx : Node3D
 
 func is_open():
 	return open
+
+func _process(delta):
+	open_gfx.visible = open
+	closed_gfx.visible = !open
