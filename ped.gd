38932ae7e4ae796ab364c9772eeb0ddbc042a238
Giovanni Karra <karragiovanni@gmail.com>
2025-01-26 14:24:58 +0100

    ULTIMAAAAAATE

--- ped.gd
+++ ped.gd
@@ -3,10 +3,12 @@ extends Node3D
 
 @onready var holder := $item_holder
 @onready var item: Weapon
+@onready var ultimate := load("res://Weapons/ultimate.tres")
 
 func put_item(new_item: Weapon):
 	if new_item == null: return
 	if %QuestMan.check_ped():
+		new_item = ultimate
 		print("OMGOMGOMGOMGOMGOMOG")
 	item = new_item
 	holder.add_child(item.gfx.instantiate())
