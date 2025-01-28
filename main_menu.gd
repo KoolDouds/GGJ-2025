ad96206e0c591cae5a7c53551eabeeb22c9587c0
Caberu <lecaberudo@gmail.com>
2025-01-28 03:36:12 +0100

    cam glictch fix

--- main_menu.gd
+++ main_menu.gd
@@ -3,13 +3,11 @@ extends Control
 @onready var game := load("res://game.tscn")
 
 func start_game():
-	print("start")
 	Engine.time_scale = 1
 	LoadingManager.load_scene(game)
 
 
 func restart():
-	print("restart")
 	Engine.time_scale = 1
 	LoadingManager.load_scene(game)
 	queue_free()
